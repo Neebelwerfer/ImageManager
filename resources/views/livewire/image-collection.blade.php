247d975e94eb1ed19d9e059d0152b7c92a3e1cdd
Neebelwerfer <jakob.hedeby@gmail.com>
2024-09-08 06:39:09 +0200

    Updated image-collection page

--- resources/views/livewire/image-collection.blade.php
+++ resources/views/livewire/image-collection.blade.php
@@ -1,5 +1,13 @@
-<div class="flex flex-row mx-4 my-4 space-x-3 auto-rows-min">
+<x-slot name="header">
+    <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        {{ __('Images') }}
+    </h2>
+</x-slot>
+
+<div class="flex flex-row flex-grow flex-shrink-0 mx-4 my-4 space-x-2 auto-rows-min">
     @foreach ($this->images as $image)
-        <img width="200" height="200" src="{{ asset($image->thumbnail_path) }}" alt="{{ $image->name }}">
+        <a class="transition ease-in-out delay-75 border border-gray-700 hover:scale-110" style="width: 256px; height: 300px" href="#">
+            <img style="width: 256px; height: 300px;" src="{{ asset($image->thumbnail_path) }}" alt="{{ $image->name }}">
+        </a>
     @endforeach
 </div>
