d048366b7dd0f463477ea61bc0583fd1a10b22d5
Neebelwerfer <jakob.hedeby@gmail.com>
2024-09-24 05:37:59 +0200

    Think image overflow is fixed

--- resources/views/layouts/collection.blade.php
+++ resources/views/layouts/collection.blade.php
@@ -6,14 +6,14 @@
         </x-slot>
     @endif
 
-    <div class="flex flex-row flex-grow min-h-full">
+    <div class="flex flex-row flex-grow">
         <x-sidebar>
             <x-sidebar.link-button route="{{ route('collection.show', 'albums') }}">Albums</x-sidebar.button>
             <x-sidebar.link-button route="{{ route('collection.show', 'categories') }}">Categories</x-sidebar.button>
             <x-sidebar.link-button route="{{ route('collection.show', 'images') }}">Images</x-sidebar.button>
         </x-sidebar>
 
-        <div class="flex justify-center flex-grow">
+        <div class="flex justify-center w-full">
             <div class="flex flex-col flex-grow-0 w-full">
                 {{ $slot }}
             </div>
--- resources/views/livewire/collection/show/grid-and-single.blade.php
+++ resources/views/livewire/collection/show/grid-and-single.blade.php
@@ -1,4 +1,4 @@
-<div class="relative overflow-hidden" x-data="{ showOptions: $wire.entangle('showOptions') }">
+<div class="relative h-full overflow-hidden" x-data="{ showOptions: $wire.entangle('showOptions') }">
 
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
@@ -10,56 +10,60 @@
         <livewire:image-show.options :image="$singeleImage"/>
     @endif
 
-    <div class="flex flex-row justify-between">
-        <div class="flex flex-row mx-2 mt-2">
-            <button
-                class="p-1 border rounded  @if($gridView) bg-slate-400 dark:bg-gray-500 @else bg-slate-600 dark:bg-gray-700 @endif hover:bg-gray-400 hover:dark:bg-gray-500"
-                wire:click="setGridView(true)" @if($gridView) disabled @endif>Grid</button>
-            <button
-                class="p-1 border rounded @if(!$gridView) bg-slate-400 dark:bg-gray-500 @else bg-slate-600 dark:bg-gray-700 @endif hover:bg-gray-400 hover:dark:bg-gray-500"
-                wire:click="setGridView(false)" @if(!$gridView) disabled @endif>Single</button>
-        </div>
-        <div class="mt-2 mr-4 @if($gridView) hidden @endif">
-            <button x-on:click="$wire.showOptions = !$wire.showOptions" class="p-1 border rounded bg-slate-600 dark:bg-gray-700 hover:bg-gray-400 hover:dark:bg-gray-500">Details</button>
-        </div>
-    </div>
-
-
-
-    <div class="@if($gridView) flex  @else collapse @endif justify-center">
-        <div class="flex flex-col justify-center justify-items-center">
-            <x-grid>
-                <x-slot name="header">
-                    <div>
-                        <form wire:submit.prevent="filter">
-                            <div>
-                                <label for="rating" class="">Minimum Rating</label>
-                                <input type="number" wire:model='minRating' name="rating" class="text-black" min=0 max=10>
-                            </div>
-                            <button type="submit">Filter</button>
-                        </form>
-                    </div>
-                </x-slot>
-
-                @foreach ($this->images as $key => $image)
-                    <x-grid.image-card-button :image="$image" x-on:click="$wire.show({{ $key }})" wire:key='grid-{{ $image->uuid }}'>
-                    </x-grid.image-card-button>
-                @endforeach
-            </x-grid>
-        </div>
-    </div>
-
-
-    @if (!$gridView && isset($singeleImage))
-        {{-- <button wire:click="previousImage" class="absolute z-30 mt-2 left-0 w-20 h-full @if(!$this->gotPrevious()) bg-gray-600 @else bg-gray-900 hover:bg-gray-700 @endif border-l border-y"><</button>
-        <button wire:click="nextImage" class="absolute z-30 mt-2 right-0 w-20 h-full @if(!$this->gotNext()) bg-gray-600 @else bg-gray-900 hover:bg-gray-700 @endif border-r border-y">></button> --}}
-
-        <div class="flex flex-col justify-center w-full h-full mt-2"  x-on:keyup.left.window="$wire.previousImage()" x-on:keyup.right.window="$wire.nextImage()">
-            <div class="flex flex-row justify-center h-full">
-                <div class="flex justify-center ">
-                    <livewire:image classes="flex justify-center w-4/6 h-full" :image="$singeleImage"/>
-                </div>
+    <div class="relative flex flex-col h-full">
+        <div class="flex flex-row justify-between">
+            <div class="flex flex-row mx-2 mt-2">
+                <button
+                    class="p-1 border rounded  @if($gridView) bg-slate-400 dark:bg-gray-500 @else bg-slate-600 dark:bg-gray-700 @endif hover:bg-gray-400 hover:dark:bg-gray-500"
+                    wire:click="setGridView(true)" @if($gridView) disabled @endif>Grid</button>
+                <button
+                    class="p-1 border rounded @if(!$gridView) bg-slate-400 dark:bg-gray-500 @else bg-slate-600 dark:bg-gray-700 @endif hover:bg-gray-400 hover:dark:bg-gray-500"
+                    wire:click="setGridView(false)" @if(!$gridView) disabled @endif>Single</button>
+            </div>
+            <div class="mt-2 mr-4 @if($gridView) hidden @endif">
+                <button x-on:click="$wire.showOptions = !$wire.showOptions" class="p-1 border rounded bg-slate-600 dark:bg-gray-700 hover:bg-gray-400 hover:dark:bg-gray-500">Details</button>
             </div>
         </div>
-    @endif
+
+
+
+        <div class="@if($gridView) flex  @else collapse @endif justify-center">
+            <div class="flex flex-col justify-center justify-items-center">
+                <x-grid>
+                    <x-slot name="header">
+                        <div>
+                            <form wire:submit.prevent="filter">
+                                <div>
+                                    <label for="rating" class="">Minimum Rating</label>
+                                    <input type="number" wire:model='minRating' name="rating" class="text-black" min=0 max=10>
+                                </div>
+                                <button type="submit">Filter</button>
+                            </form>
+                        </div>
+                    </x-slot>
+
+                    @foreach ($this->images as $key => $image)
+                        <x-grid.image-card-button :image="$image" x-on:click="$wire.show({{ $key }})" wire:key='grid-{{ $image->uuid }}'>
+                        </x-grid.image-card-button>
+                    @endforeach
+                </x-grid>
+            </div>
+        </div>
+
+
+        @if (!$gridView && isset($singeleImage))
+            <div class="h-full">
+                <button wire:click="previousImage" class="absolute z-30 mt-2 left-0 w-20 h-full @if(!$this->gotPrevious()) bg-gray-600 @else bg-gray-900 hover:bg-gray-700 @endif border-l border-y"><</button>
+                <button wire:click="nextImage" class="absolute z-30 mt-2 right-0 w-20 h-full @if(!$this->gotNext()) bg-gray-600 @else bg-gray-900 hover:bg-gray-700 @endif border-r border-y">></button>
+
+                <div class="flex flex-col justify-center w-full h-full mt-2 border-y"  x-on:keyup.left.window="$wire.previousImage()" x-on:keyup.right.window="$wire.nextImage()">
+                    <div class="flex flex-row justify-center h-full">
+                        <div class="flex justify-center ">
+                            <livewire:image classes="flex justify-center w-4/6 h-full" :image="$singeleImage"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
 </div>
