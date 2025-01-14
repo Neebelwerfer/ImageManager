9e84d9a8f113d3dfea63418c37355acf2a532789
Neebelwerfer <jakob.hedeby@gmail.com>
2025-01-14 15:06:18 +0100

    Added an option for cleaning up the temp files

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -43,12 +43,20 @@ class Dashboard extends Component
         $available = intval(disk_total_space(storage_path('app')) / 1024 / 1024 / 1024);
         $usedByImages = number_format((float)$this->GetDirectorySize(storage_path('app/images')) / 1024 / 1024, 2);
         $usedByThumbnails = number_format((float)$this->GetDirectorySize(storage_path('app/thumbnails')) / 1024 / 1024, 2);
+        $usedByTemp = number_format((float)$this->GetDirectorySize(storage_path('app/livewire-tmp')) / 1024 / 1024, 2);
 
         $used = $usedByImages + $usedByThumbnails;
 
         $percent = ($used / $available) * 100;
-        return ['used' => $used, 'free' => $available, 'percent' => $percent, 'usedByImages' => $usedByImages, 'usedByThumbnails' => $usedByThumbnails];
+        return ['used' => $used, 'free' => $available, 'percent' => $percent, 'usedByImages' => $usedByImages, 'usedByThumbnails' => $usedByThumbnails, 'usedByTemp' => $usedByTemp];
 
+    }
+
+    public function deleteTempImages(){
+        $temp = Storage::disk('local')->allFiles('livewire-tmp');
+        foreach ($temp as $file) {
+            Storage::disk('local')->delete($file);
+        }
     }
 
     public function render()
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -2,14 +2,23 @@
 
     <div class="relative flex flex-col w-full h-full">
         <div class="flex justify-center w-full">
-            <div class="grid grid-cols-5 grid-rows-4 gap-2 p-1.5 mx-4 my-2 bg-gray-800 border rounded h-full" style="width: 70%;">
-                <div class="flex flex-row justify-between p-2 rounded w-max">
+            <div class="grid grid-cols-1 grid-rows-4 gap-2 p-1.5 mx-4 my-2 bg-gray-800 border rounded h-full"
+                style="width: 70%;">
+                <div class="flex flex-col justify-between p-2 rounded w-max">
                     <ul class="w-full space-y-2">
-                        <li>Users: {{ $this->UserCount()}}</li>
-                        <li>Images: {{ $this->ImageCount()}}</li>
-                        <li>Storage available: {{ $this->SizeUsage()['free']}} GB</li>
-                        <li>Storagespace used by images: {{ $this->SizeUsage()['usedByImages']}} MB</li>
-                        <li>Storagespace used by thumbnails: {{ $this->SizeUsage()['usedByThumbnails']}} MB</li>
+                        <li><p class="p-1">Users: {{ $this->UserCount() }}</p></li>
+                        <li><p class="p-1">Images: {{ $this->ImageCount() }}</p></li>
+                        <li><p class="p-1">Storage available: {{ $this->SizeUsage()['free'] }} GB</p></li>
+                        <li><p class="p-1">Storagespace used by images: {{ $this->SizeUsage()['usedByImages'] }} MB</p></li>
+                        <li><p class="p-1">Storagespace used by thumbnails: {{ $this->SizeUsage()['usedByThumbnails'] }} MB</p></li>
+                        <li>
+                            <div class="flex flex-row">
+                                <p class="p-1">Storagespace used by temporary images: {{ $this->SizeUsage()['usedByTemp'] }} MB </p>
+                                <button class="p-1 ml-2 border rounded bg-red-600/90" wire:click="deleteTempImages()"
+                                    wire:confirm="Are you sure you want to delete all temporary images? It might brick ongoing upload session">Delete
+                                    temporary images</button>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
