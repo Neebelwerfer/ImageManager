587334e5aedac390de9596f85446455df75d2653
Neebelwerfer <jakob.hedeby@gmail.com>
2025-02-17 17:31:02 +0100

    Found the fix for enabling dark mode/light mode

--- resources/views/components/grid.blade.php
+++ resources/views/components/grid.blade.php
@@ -5,7 +5,7 @@
         </div>
     @endif
 
-    <div class="grid grid-cols-5 grid-rows-4 gap-2 p-1.5 mx-4 my-2 bg-gray-800 border rounded h-full" style="width: 97.5%;">
+    <div class="grid grid-cols-5 grid-rows-4 gap-2 p-1.5 mx-4 my-2 bg-slate-300 dark:bg-gray-800 border border-black dark:border-white rounded h-full" style="width: 97.5%;">
        {{ $slot }}
     </div>
 </div>
--- resources/views/components/sidebar/link-button.blade.php
+++ resources/views/components/sidebar/link-button.blade.php
@@ -1,12 +1,3 @@
-@props(['active' => false, 'route' => '#'])
+@props(['route' => '#'])
 
-@php
-
-    $classes =
-        $active ?? false
-            ? 'w-full flex justify-center p-1 block border-t border-b hover:bg-gray-400 hover:dark:bg-gray-500 bg-gray-400 dark:bg-gray-500'
-            : 'w-full flex justify-center p-1 block border-t border-b hover:bg-gray-400 hover:dark:bg-gray-500';
-
-@endphp
-
-<li><a {{ $attributes->merge(['class' => $classes, 'href' => $route]) }} wire:navigate>{{ $slot }}</a></li>
+<li><a {{ $attributes->merge(['class' => 'w-full flex justify-center p-1 block border-t border-b hover:bg-gray-400 hover:dark:bg-gray-500', 'href' => $route]) }} wire:navigate wire:current='bg-gray-400 dark:bg-gray-500'>{{ $slot }}</a></li>
