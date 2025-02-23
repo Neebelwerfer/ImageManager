c70bf1a5e868a8a1a80ce43585c69e03b0a9ca75
Neebelwerfer <jakob.hedeby@gmail.com>
2025-02-23 23:50:25 +0100

    Increased timeout time to 5 minutes  to handle big uploads better

--- app/Jobs/Upload/ProcessUpload.php
+++ app/Jobs/Upload/ProcessUpload.php
@@ -7,18 +7,24 @@ use App\Models\Upload;
 use App\Models\User;
 use App\Services\ImageService;
 use App\Support\Enums\UploadStates;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueueAfterCommit;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\ImageManager;
 use Illuminate\Support\Str;
 
 
-class ProcessUpload implements ShouldQueue
+class ProcessUpload implements ShouldQueue, ShouldQueueAfterCommit, ShouldBeUnique
 {
     use Queueable;
+
+
+    public $timeout = 300;
 
     /**
      * Create a new job instance.
@@ -27,10 +33,7 @@ class ProcessUpload implements ShouldQueue
         public readonly User $user,
         public readonly Upload $upload,
         public readonly array $data
-    )
-    {
-
-    }
+    ){}
 
     /**
      * Execute the job.
@@ -40,26 +43,48 @@ class ProcessUpload implements ShouldQueue
         $imageService = app(ImageService::class);
 
         $imageUploads = [];
-        foreach ($this->data as $key => $data)
+        DB::beginTransaction();
+        try {
+            //code...
+            foreach ($this->data as $key => $data)
+            {
+                $img = ImageManager::gd()->read(file_get_contents($data['path']));
+
+                $upload = new ImageUpload(
+                    [
+                        'uuid' => str::uuid(),
+                        'upload_ulid' => $this->upload->ulid,
+                        'user_id' => $this->user->id,
+                        'extension' => $data['extension'],
+                        'hash' => $imageService->createImageHash($img->core()->native())
+                    ]);
+                $upload->save();
+                $imageUploads[$key] = $upload;
+
+                $cryptImage = Crypt::encrypt((string) $img->encodeByMediaType(), false);
+                Storage::disk('local')->put('temp/'. $upload->uuid, $cryptImage);
+                unlink($data['path']);
+            }
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return;
+        }
+        DB::commit();
+        $this->upload->setState(UploadStates::Waiting);
+    }
+
+    public function failed() : void
+    {
+        DB::rollBack();
+
+        foreach ($this->data as $data)
         {
-            $img = ImageManager::gd()->read(file_get_contents($data['path']));
-
-            $upload = new ImageUpload(
-                [
-                    'uuid' => str::uuid(),
-                    'upload_ulid' => $this->upload->ulid,
-                    'user_id' => $this->user->id,
-                    'extension' => $data['extension'],
-                    'hash' => $imageService->createImageHash($img->core()->native())
-                ]);
-            $upload->save();
-            $imageUploads[$key] = $upload;
-
-            $cryptImage = Crypt::encrypt((string) $img->encodeByMediaType(), false);
-            Storage::disk('local')->put('temp/'. $upload->uuid, $cryptImage);
             unlink($data['path']);
         }
+    }
 
-        $this->upload->setState(UploadStates::Waiting);
+    public function uniqueId(): string
+    {
+        return $this->user->id;
     }
 }
